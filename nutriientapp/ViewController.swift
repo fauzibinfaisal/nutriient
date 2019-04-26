257ffe4ffd9603205aacbb89a665527f384914bd
Fauzi <fauzibinfaisal@gmail.com>
2019-04-26 14:18:37 +0700

    Initial Commit

---
+++ nutriientapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  nutriientapp
+//
+//  Created by Fauzi Fauzi on 26/04/19.
+//  Copyright © 2019 Kelompok8. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
